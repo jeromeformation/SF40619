4c2c08aeb3e72c0757d0231a9593884e6b519b03
jerome <ambroise.jerome.formation@gmail.com>
2019-07-02 12:05:33 +0200

    Liste des utilisateurs en back-office

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,16 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class AdminController
+ * @Route("/admin")
+ */
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/", name="admin_index")
      */
     public function index()
     {
         return $this->render('admin/index.html.twig');
+    }
+
+    /**
+     * @Route("/users")
+     * @param UserRepository $repository
+     * @return Response
+     */
+    public function listUser(UserRepository $repository): Response
+    {
+        return $this->render('admin/user/list.html.twig', [
+            'users' => $repository->findAll()
+        ]);
     }
 }
